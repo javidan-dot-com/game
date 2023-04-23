6b4595dda705382eb7e6b57ae73fd5d88ac36104
wwJavid <javababayev@gmail.com>
2023-04-23 15:29:29 +0300

    build: start fresh for game page

--- components/player-info/player-info.tsx
+++ components/player-info/player-info.tsx
@@ -1,3 +1,6 @@
+import { CommonStoreContext } from "@/stores/common.store";
+import { useContext } from "react";
+
 export type TPlayerInfo = {
     player: string,
     playerName: string,
@@ -9,10 +12,14 @@ interface PlayerInfoProps {
 }
 
 const PlayerInfo = ({ players }: PlayerInfoProps) => {
+    const { startFresh } = useContext(CommonStoreContext);
+
     return (
         <div>
             <h3>Player 1: {players[0].playerName}</h3>
             <h3>Player 2: {players[1].playerName}</h3>
+
+            <button onClick={startFresh}>Start from Fresh</button>
         </div>
     )
 }
